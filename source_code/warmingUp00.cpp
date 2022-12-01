78b06cab8c5e9fc0fde0f6264c28fe1a09f90c9e
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-01 22:10:47 +0200

    Create warmingUp00.cpp

---
+++ source_code/warmingUp00.cpp
@@ -0,0 +1,51 @@
+// warmingUp00.cpp
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+    vector<string> team;
+  
+    team.push_back("Takis");
+    team.push_back("Vivi");
+
+    // +1 push to vector
+    
+    // +1 push to vector
+  
+    // +1 push to vector
+    
+    // +1 push to vector
+  
+    // +1 push to vector
+    
+    // +1 push to vector
+  
+    // +1 push to vector
+    
+    // +1 push to vector
+  
+    // +1 push to vector
+    
+    // +1 push to vector
+  
+    // +1 push to vector
+    
+    // +1 push to vector
+  
+    // +1 push to vector
+    
+    // +1 push to vector
+    
+
+    // show on the screen
+    for (auto i = team.rbegin(); i != team.rend(); i++)
+        cout << *i << endl;
+
+    return 0;
+}
