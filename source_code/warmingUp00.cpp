6865c8057e532dec24cc584e99b1dcf9ecd2b262
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-03 18:46:49 +0200

    Update warmingUp00.cpp

--- source_code/warmingUp00.cpp
+++ source_code/warmingUp00.cpp
@@ -14,46 +14,6 @@ int main() {
     team.push_back("Takis");
     team.push_back("Vivi");
 
-    team.push_back("Spiros");
-    
-    // +1 push to vector
-  
-    team.push_back("Iordanis");
-    
-    team.push_back("Maria");
-  
-    // +1 push to vector
-    
-    team.push_back("gpnb");
-  
-    team.push_back("Efthymis");
-    
-    team.push_back("oxiKafritsas");
-    team.push_back("sokratis");
-    // +1 push to vector
-    
-    team.push_back("Vaghelis");  
-    
-    // +1 push to vector
-    
-    // +1 push to vector
-    team.push_back("Vivi");
-  
-    team.push_back("Alex");
-    team.push_back("Petros");
-    
-
-    team.push_back("Themos");
-
-    
-    
-
-    
-    team.push_back("Iason");     
-    team.push_back("Kostas");  
-  
-    team.push_back("test for fork");     
-
     // show on the screen
     for (auto i = team.rbegin(); i != team.rend(); i++)
         cout << *i << endl;
