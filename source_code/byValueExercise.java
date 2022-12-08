ea3b04b2a48567bca4c6724c8e04c40caeff7a99
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-12-08 20:24:26 +0200

    Create byValueExercise.java

---
+++ source_code/byValueExercise.java
@@ -0,0 +1,35 @@
+// byValueExercise.java
+// rename to Main.java
+
+
+package aloni.main; // check that the package name is correct
+
+public class Main {
+ 
+   public static void main(String[] args){
+
+   int num = 10;   
+   System.out.println(num);
+   
+   // >>>>>>>>>>>  01  >>>>>>>>>>>
+   // add the proper source code
+   // in order to change to increase
+   // the value of num with a method call
+   
+   
+   // <<<<<<<<<<  01  <<<<<<<<<<
+   
+   System.out.println(num);
+   
+}
+   
+   // >>>>>>>>>>>  02  >>>>>>>>>>>
+   // you can change the method below
+   // in ay way you want
+   static void increase(int x)
+    {
+        x += 1;
+    }
+   // <<<<<<<<<<  02  <<<<<<<<<<   
+   
+} // end of class Main
